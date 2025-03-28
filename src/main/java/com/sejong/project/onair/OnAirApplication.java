d52fd533f4d8c0ddc83e0e72c6863449ed26bf64
yongsik <wkdrndydtlr@naver.com>
2025-03-28 15:06:32 +0900

    Initial commit

---
+++ src/main/java/com/sejong/project/onair/OnAirApplication.java
@@ -0,0 +1,13 @@
+package com.sejong.project.onair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnAirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnAirApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sejong/project/onair/OnAirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sejong.project.onair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnAirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
