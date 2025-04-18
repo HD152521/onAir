d50664997aa889a69c50322e40e20725d889dc27
yongsik <wkdrndydtlr@naver.com>
2025-04-18 14:05:42 +0900

    (feat) member domain설정 완료

---
+++ src/main/java/com/sejong/project/onair/domain/member/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.sejong.project.onair.domain.member.repository;
+
+import com.sejong.project.onair.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+}
---
+++ src/main/java/com/sejong/project/onair/domain/member/service/MemberService.java
@@ -0,0 +1,8 @@
+package com.sejong.project.onair.domain.member.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface MemberService {
+}
