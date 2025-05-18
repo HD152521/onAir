95cf3eae5a2fe39ce22e4160bd97be82715e41dc
yongsik <wkdrndydtlr@naver.com>
2025-05-18 12:16:04 +0900

    (fix) health체크용 추가

--- src/main/java/com/sejong/project/onair/domain/TestController.java
+++ src/main/java/com/sejong/project/onair/domain/TestController.java
@@ -2,15 +2,20 @@ package com.sejong.project.onair.domain;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-
 public class TestController {
-    @RequestMapping("/test")
+    @GetMapping("/test")
     public ResponseEntity<String> test(){
+        return ResponseEntity.ok("OK");
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health(){
         return ResponseEntity.ok("OK");
     }
 }
--- src/main/java/com/sejong/project/onair/global/filter/AuthenticationTokenFilter.java
+++ src/main/java/com/sejong/project/onair/global/filter/AuthenticationTokenFilter.java
@@ -36,7 +36,8 @@ public class AuthenticationTokenFilter extends OncePerRequestFilter {
                                     FilterChain filterChain) throws ServletException, IOException {
         //path보고 안봐도 되는거 거르기
         String path = request.getServletPath();
-        if (path.startsWith("/login") || path.startsWith("/signup") || path.startsWith("/swagger") || path.startsWith("/observatory")|| path.startsWith("/test")) {
+        if (path.startsWith("/login") || path.startsWith("/signup") || path.startsWith("/swagger") || path.startsWith("/observatory")||
+                path.startsWith("/test") || path.startsWith("/health")) {
             filterChain.doFilter(request, response);
             return;
         }
